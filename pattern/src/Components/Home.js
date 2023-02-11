9164d201937b8570a29e95ae3eef65cb5c5369da
suchethaleonidou <Suchethao>
2023-02-11 12:24:49 -0500

    Created one button for next with a placeholder

---
+++ pattern/src/Components/Home.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [
+    "https://picsum.photos/200/300?image=10",
+    "https://picsum.photos/200/300?image=20",
+    "https://picsum.photos/200/300?image=30"
+  ];
+
+    const nextImage = () => {
+      setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    };
+
+  return (
+      <div>
+        <img src={images[currentImageIndex]} alt="slideshow" />
+        <button onClick={nextImage}>Next</button>
+      </div>
+    );
+  };
+
+  export default Home;
+  
+  
